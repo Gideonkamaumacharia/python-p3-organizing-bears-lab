913834ad029a459586a6fb047e32565e785800af
Gideon Macharia <kamaugedion95@gmail.com>
2024-06-04 22:03:06 +0300

    complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(1,'Mr. Chocolate',4,'M','black','aggressive',True);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(2,'Rowdy',7,'F','brown','protective',True);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(3,'Tabitha',2,'M','black','curious',True);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(4,'Sergeant Brown',14,'F','blonde','calm',False);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(5,'Melissa',5,'F','red','timid',False);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(6,'Grinch',4,'M','white','adaptive',True);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(7,'Wendy',8,'F','brown','dominant',False);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES(8,NULL,NULL,'M','white','dominant',True);
